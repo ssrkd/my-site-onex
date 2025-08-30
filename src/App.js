658761e98a14524f0166c94c6c0bf42609d84419
srk451 <seriksisembaev.d@gmail.com>
2025-08-30 23:21:13 +0500

    srk

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 
 function App() {
   return (
-    <Router basename="/my-site-onex">
+    <Router>
       <Routes>
-        {/* редирект с корня на Home */}
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
