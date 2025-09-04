813e5ec559935dfede31f103aad277ea215d569a
srk451 <seriksisembaev.d@gmail.com>
2025-09-04 11:47:09 +0500

    srk

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // src/App.js
 import React from "react";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./components/home";
 import Dostavka from "./components/dostavka";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dostavka" element={<Dostavka />} />
